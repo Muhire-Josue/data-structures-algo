27dfbb33809f21c1bb1427686ec3324d6a7fe403
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-05-06 12:56:20 +0200

    O(1) or constant time

--- big-o/basics.js
+++ big-o/basics.js
@@ -1,14 +1,20 @@
-const data = ['Memo']
+const data = ["Memo", "Memo", "Memo"];
 
+//O(n)
 function findMemo(array) {
-  const t0 = performance.now()
+  const t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'Memo') {
-      console.log('Memo found!')
+    if (array[i] === "Memo") {
+      console.log("Memo found!");
     }
   }
-  const t1 = performance.now()
-  console.log('Call to find memo took ' + (t1 - t0) + ' milliseconds')
+  const t1 = performance.now();
+  console.log("Call to find memo took " + (t1 - t0) + " milliseconds");
 }
 
-findMemo(data)
+// O(1)
+function findFirstElement(array) {
+  console.log(array[0]);
+}
+
+findMemo(data);
