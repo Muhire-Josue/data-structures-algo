4b36bccc90d560514dbbfbee37d073ae050b43b6
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-05-23 12:18:51 +0200

    - Adds insert, doubly linkedlist

--- linkedlist/doubly.js
+++ linkedlist/doubly.js
@@ -1,10 +1,10 @@
 class DoublyNode {
     constructor(value) {
-      this.value = value;
-      this.next = null;
-      this.prev = null;
+        this.value = value;
+        this.next = null;
+        this.prev = null;
     }
-  }
+}
 
 class DoublyLinkedList {
     constructor(value) {
@@ -15,25 +15,57 @@ class DoublyLinkedList {
         };
         this.tail = this.head;
         this.length++;
-        return this;
+        return this.printList();
     }
 
-    append(value){
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+          array.push(currentNode.value)
+          currentNode = currentNode.next
+        }
+        console.log(array);
+      }
+
+    append(value) {
         const newNode = new DoublyNode(value);
         newNode.prev = this.tail;
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
-        return this;
+        return this.printList();
     }
 
-    prepend(value){
+    prepend(value) {
         const newNode = new DoublyNode(value);
         this.head.prev = newNode;
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
-        return this;
+        return this.printList();
+    }
+
+    insert(value, index) {
+        let newNode = new DoublyNode(value);
+        let leader = this.traverseToIndex(index - 1);
+        let follower = leader.next;
+        leader.next = newNode;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+        return this.printList();
+
+    }
+    traverseToIndex(index) {
+        //Check parameters
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
     }
 }
 
@@ -41,6 +73,6 @@ const myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(6);
 myLinkedList.prepend(1);
-// myLinkedList.insert(99, 3);
+myLinkedList.insert(99, 2);
 // myLinkedList.remove(4);
-console.log(myLinkedList);
+// console.log(myLinkedList);
