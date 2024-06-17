3c9ddb11dbed5c0c0d79c25a58e298abbfa6a3fe
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-06-17 11:35:34 +0200

    - Adds basic code for implementing a stack

---
+++ queue/index.js
@@ -0,0 +1,24 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+
+    }
+    push() {
+        
+    }
+    pop() {
+        
+    }
+}
