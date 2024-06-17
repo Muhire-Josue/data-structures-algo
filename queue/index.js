e95f9a790ffdd3b52cdd77245d35f994c114129e
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-06-17 12:01:17 +0200

    - Stack, push()

--- queue/index.js
+++ queue/index.js
@@ -1,12 +1,12 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
 
 class Stack {
-    constructor(){
+    constructor() {
         this.top = null;
         this.bottom = null;
         this.length = 0;
@@ -15,12 +15,25 @@ class Stack {
     peek() {
 
     }
-    push() {
-        
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const temp = this.top;
+            this.top = newNode;
+            this.top.next = temp;
+        }
+        this.length++;
+        return this;
     }
     pop() {
-        
+
     }
 }
 
 const myStack = new Stack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack);
