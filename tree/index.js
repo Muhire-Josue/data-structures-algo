bc1ea89e722ade06429f7344c03e684fec636878
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-06-19 11:41:36 +0200

    - Nothing here

--- tree/index.js
+++ tree/index.js
@@ -11,11 +11,35 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    insert() {
-
+    insert(value) {
+        const newnode = new Node(value);
+        if(this.root === null) {
+            this.root = newnode;
+        }
     }
 
     lookUp() {
-        
+
     }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+// tree.insert(4);
+// tree.insert(6);
+// tree.insert(20);
+// tree.insert(170);
+// tree.insert(15);
+// tree.insert(1);
+// tree.remove(170);
+console.log(JSON.stringify(traverse(tree.root)));
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
 }
