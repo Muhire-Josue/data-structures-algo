b3541d77f238b5506176954698c169c86763220a
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-06-24 10:43:56 +0200

    - Factorial, findFactorialIterative()

---
+++ algorithms/index.js
@@ -0,0 +1,17 @@
+function findFactorialRecursive(number) {
+
+}
+
+function findFactorialIterative(number) {
+    let answer = 1;
+    if (number === 2) {
+        answer = 2;
+    }
+
+    for (let i = 2; i <= number; i++) {
+        answer = answer * i;
+    }
+    return answer;
+}
+
+console.log(findFactorialIterative(5));
