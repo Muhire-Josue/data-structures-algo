666c0691350554758e6871b6bb16116c198e0930
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-08 20:34:57 +0200

    - Implements bubble sort

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -2,6 +2,15 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
     //Code here
+    for(let j=0; j < array.length; j++) {
+        for(let i=0; i < array.length; i++) {
+            if(array[i] > array[i+1]) {
+                let temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
+            }
+        }
+    }
 }
 
 bubbleSort(numbers);
