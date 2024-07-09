6e4c0df0333dee6632e04294e15b98883b6e9118
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-09 11:04:35 +0200

    - Implements selectionSort

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -15,9 +15,21 @@ function bubbleSort(array) {
 
 function selectionSort(array) {
     //Code Here
+    for (let i = 0; i < array.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+        }
+        let temp = array[i];
+        array[i] = array[min];
+        array[i + 1] = temp;
+    }
+    return array;
 }
 
 console.log(selectionSort(numbers));
 
-bubbleSort(numbers);
-console.log(numbers);
+// bubbleSort(numbers);
+// console.log(numbers);
