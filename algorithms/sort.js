05b612490ab4f8726ba9de64ed3d8fa34be4d6bc
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-09 11:08:48 +0200

    - Fix out of memory error, selectionSort()

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -17,14 +17,15 @@ function selectionSort(array) {
     //Code Here
     for (let i = 0; i < array.length; i++) {
         let min = i;
+        let temp = array[i];
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[min]) {
                 min = j;
             }
         }
-        let temp = array[i];
+        
         array[i] = array[min];
-        array[i + 1] = temp;
+        array[min] = temp;
     }
     return array;
 }
