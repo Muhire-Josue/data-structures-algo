0676d82dd021a5aa85a91389932216bff4400d8b
Muhire-Josue <rutayisire.josue09@gmail.com>
2024-07-09 10:29:06 +0200

    - selectionSort(), initialize

--- algorithms/sort.js
+++ algorithms/sort.js
@@ -2,16 +2,22 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
     //Code here
-    for(let j=0; j < array.length; j++) {
-        for(let i=0; i < array.length; i++) {
-            if(array[i] > array[i+1]) {
+    for (let j = 0; j < array.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
                 let temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
             }
         }
     }
 }
+
+function selectionSort(array) {
+    //Code Here
+}
+
+console.log(selectionSort(numbers));
 
 bubbleSort(numbers);
 console.log(numbers);
